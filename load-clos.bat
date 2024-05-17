1f65d4dbc1ec4f4c4333a39acd4d16fae51b3e5b
Briter Gonzalez <briter.gd@gmail.com>
2024-05-17 23:00:48 +0200

    feat(clos): generate set

--- load-clos.bat
+++ load-clos.bat
@@ -1,3 +1,5 @@
 sbcl --noinform ^
 --load src/tests/loader.lisp ^
+--eval "(load \"~/quicklisp/setup.lisp\")" ^
+--eval "(ql:quickload \"closer-mop\")" ^
 %*
--- src/functions/functions.lisp
+++ src/functions/functions.lisp
@@ -1,8 +1,15 @@
 (in-package :closless)
 
+(defun concat-sets (set1 set2)
+  (reduce (lambda (acc x) (adjoin x acc)) set1 :initial-value set2))
+
 (defun make-instance (class)
   (make-object :class (symbol-value class)))
 
+(defun find-class (class-symbol)
+  (symbol-value class-symbol))
 
 (defun generate-class-set (class)
-  (class-direct-superclass class))
+  (cond
+   ((eql class *object*) ())
+   (t (reduce (lambda (acc x) (concat-sets (generate-class-set (find-class x)) (adjoin x acc))) (class-direct-superclass class) :initial-value ()))))
