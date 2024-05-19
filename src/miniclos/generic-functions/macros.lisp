7f5ffcc0979fae6006f9a396a4af70db85ce3c2e
Briter Gonzalez <briter.gd@gmail.com>
2024-05-19 22:33:04 +0200

    feat(clos): add method helper

--- src/miniclos/generic-functions/macros.lisp
+++ src/miniclos/generic-functions/macros.lisp
@@ -24,23 +24,26 @@
             (remove ,next-most-specific-method ,next-methods))
           (error 'generic-function-error :message "There is no available next method")))))
 
-(defun defmethod-normal (name arguments body)
+(defun defmethod-helper (name arguments function-body)
   (let ((new-num-args (length arguments))
-        (num-args (generic-function-num-args (symbol-value name)))
-        (args-mapper (extract-specializers-variables arguments)))
+        (num-args (generic-function-num-args (symbol-value name))))
           (cond
             ((not (eql new-num-args num-args))
               `(error 'generic-function-error :message (format nil "Invalid number of arguments: ~d" ,new-num-args)))
-            (t
-              `(add-method ,name
-                        (make-method
-                        :specializers ,(cons 'list (cadr args-mapper))
-                        :function ,(with-gensyms (lambda-arguments lambda-next-methods)
-                                    `(lambda (,lambda-arguments ,lambda-next-methods)
-                                      (labels ((call-next-method () ,(next-method-helper lambda-arguments lambda-next-methods)))
-                                        ,(append
-                                          `(let ,(loop for variable-name in (car args-mapper) for index from 0 collect `(,variable-name (nth ,index ,lambda-arguments))))
-                                          body))))))))))
+            (t function-body))))
+
+(defun defmethod-normal (name arguments body)
+  (defmethod-helper name arguments
+      (let ((args-mapper (extract-specializers-variables arguments)))
+        `(add-method ,name
+          (make-method
+          :specializers ,(cons 'list (cadr args-mapper))
+          :function ,(with-gensyms (lambda-arguments lambda-next-methods)
+                      `(lambda (,lambda-arguments ,lambda-next-methods)
+                        (labels ((call-next-method () ,(next-method-helper lambda-arguments lambda-next-methods)))
+                          ,(append
+                            `(let ,(loop for variable-name in (car args-mapper) for index from 0 collect `(,variable-name (nth ,index ,lambda-arguments))))
+                            body)))))))))
 
 (defmacro defmethod (name &rest all-arguments)
   (let ((first (car all-arguments))
