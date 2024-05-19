3b7afb5ed5d3d58e9638e0edc5d67cfda99c3f54
Briter Gonzalez <briter.gd@gmail.com>
2024-05-19 14:16:29 +0200

    feat(clos): create macro for defmethod

--- src/miniclos/generic-functions/macros.lisp
+++ src/miniclos/generic-functions/macros.lisp
@@ -1,4 +1,30 @@
 (in-package :closless)
+(load "src/utils/lists.lisp")
+(load "src/utils/with-gensyms.lisp")
 
 (defmacro defgeneric (name)
   `(defvar ,name (make-generic-function)))
+
+(defmacro gen-let (names values body)
+  `(let ,(zip names values) ,body))
+
+(defun extract-specializers-variables (arguments)
+  (reduce (lambda (acc tuple)
+            (if (listp tuple)
+                `(,(cons (car tuple) (car acc)) ,(cons (cadr tuple) (cadr acc)))
+                `(,(cons tuple (car acc)) ,(cons '*object* (cadr acc)))))
+      (reverse arguments) :initial-value '(() ())))
+
+(defmacro defmethod (name arguments &rest body)
+  (let ((args-mapper (extract-specializers-variables arguments)))
+    (with-gensyms (lambda-arguments lambda-next-methods)
+                  `(add-method ,name
+                               (make-method
+                                :specializers ,(cons 'list (cadr args-mapper))
+                                :function (lambda (,lambda-arguments ,lambda-next-methods)
+                                            (declare (ignore ,lambda-next-methods))
+                                            (let ,(loop for name in (car args-mapper) for index from 0 collect `(,name (nth ,index ,lambda-arguments)))
+                                              ,(cons 'progn body))))))))
+
+
+; (print (macroexpand '(defmethod display ((person-obj person)) (print (slot-value person-obj 'name)))))
--- tests/generic-functions.lisp
+++ tests/generic-functions.lisp
@@ -16,24 +16,28 @@
 
 (defgeneric display)
 
-(add-method display
-            (make-method
-             :specializers (list person)
-             :function (lambda (arguments next-methods)
-                         (declare (ignore next-methods))
-                         (print (slot-value (car arguments) 'name))
-                         (print (slot-value (car arguments) 'address)))))
+; (add-method display
+;             (make-method
+;              :specializers (list person)
+;              :function (lambda (arguments next-methods)
+;                          (declare (ignore next-methods))
+;                          (print (slot-value (car arguments) 'name))
+;                          (print (slot-value (car arguments) 'address)))))
 
-(add-method display
-            (make-method
-             :specializers (list employee)
-             :function (lambda (arguments next-methods)
-                         (let ((next-most-specific-method
-                                (select-most-specific-method arguments next-methods)))
-                           (funcall (method-function next-most-specific-method)
-                             arguments
-                             (remove next-most-specific-method next-methods)))
-                         (print (slot-value (car arguments) 'employer)))))
+(defmethod display ((person-obj person))
+  (print (slot-value person-obj 'name))
+  (print (slot-value person-obj 'address)))
+
+; (add-method display
+;             (make-method
+;              :specializers (list employee)
+;              :function (lambda (arguments next-methods)
+;                          (let ((next-most-specific-method
+;                                 (select-most-specific-method arguments next-methods)))
+;                            (funcall (method-function next-most-specific-method)
+;                              arguments
+;                              (remove next-most-specific-method next-methods)))
+;                          (print (slot-value (car arguments) 'employer)))))
 
 (call-generic-function display p)
 (call-generic-function display e)
