61631ca056ef81d0e43b7755d06d0a56bbc8d20e
Briter Gonzalez <briter.gd@gmail.com>
2024-05-20 09:55:26 +0200

    feat(clos): add multiply example

--- tests/auxiliary-methods/example.lisp
+++ tests/auxiliary-methods/example.lisp
@@ -4,9 +4,6 @@
 
 (defclass number () (value))
 
-(defvar num (make-instance 'number))
-(setf (slot-value num 'value) 5)
-
 (defgeneric square (x))
 
 (defmethod square (x)
@@ -15,14 +12,40 @@
 (defvar logger '())
 
 (defmethod square :before (x)
-  (setf logger (append logger (list (concatenate 'string "Before square: " (write-to-string (slot-value x 'value)))))))
+  (setf logger (append logger (list (concatenate 'string "About to calculate the square of " (write-to-string (slot-value x 'value)))))))
 
 (defmethod square :after (x)
-  (setf logger (append logger (list (concatenate 'string "After square: " (write-to-string (slot-value x 'value)))))))
+  (setf logger (append logger (list (concatenate 'string "Just calculated the square of " (write-to-string (slot-value x 'value)))))))
 
+(defvar num (make-instance 'number))
+(setf (slot-value num 'value) 5)
 (assert-equals (square num) 25)
-(assert-equals logger '("Before square: 5" "After square: 5"))
+(assert-equals logger '("About to calculate the square of 5"
+                        "Just calculated the square of 5"))
 
-(unbound-variables '(<number> num square logger))
+(setf logger '())
+
+(defgeneric multiply (x y))
+
+(defmethod multiply (x y)
+  (* (slot-value x 'value) (slot-value y 'value)))
+
+(defmethod multiply :around (x y)
+  (declare (ignore x))
+  (declare (ignore y))
+  (setf logger (append logger '("Before multiplying")))
+  (let ((result (call-next-method)))
+    (setf logger (append logger '("After multiplying")))
+    result))
+
+(defvar num1 (make-instance 'number))
+(setf (slot-value num1 'value) 3)
+(defvar num2 (make-instance 'number))
+(setf (slot-value num2 'value) 5)
+(assert-equals (multiply num1 num2) 15)
+(assert-equals logger '("Before multiplying"
+                        "After multiplying"))
+
+(unbound-variables '(<number> num num1 num2 square multiply logger))
 
 (print "Auxiliary Methods | Assignment example => All the tests passed")
